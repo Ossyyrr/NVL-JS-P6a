abcff7990e255271ff57b1086d693b6b1cab2034
Patricia Manzanero Jiménez <52755910+Ossyyrr@users.noreply.github.com>
2019-08-18 20:21:23 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,5 @@
+var nombre = "Patricia";
+var edad = 100;
+var diasSemana = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sábado", "Domingo"];
+var gravedad= "9.8 m^2/s";
+var impares = [1,3,5];
